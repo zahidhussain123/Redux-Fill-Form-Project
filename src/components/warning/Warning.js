a880152037022fa9dcfe54fa392fd0cc314e4d5c
Zaid Khan <zahid4307@gmail.com>
2021-12-18 21:10:49 +0500

    changes made

--- src/components/warning/Warning.js
+++ src/components/warning/Warning.js
@@ -1,11 +1,13 @@
 
 import React from "react";
+import { useSelector } from "react-redux";
 import "./warning.css";
 
 export default function Warning() {
+  const name = useSelector(state => state.user.name)
   return (
     <div className="warning">
-      Deleting account cannot be undone <b>Zahid</b>! You should confirm your
+      Deleting account cannot be undone <b>{name}</b>! You should confirm your
       password to delete your account.
     </div>
   );
